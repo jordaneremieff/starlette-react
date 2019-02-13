3e6f510a7a1d9e9462f3203b6552d0f50f6de0ac
Ivan Rudovol <ivan.rudovol@gmail.com>
2019-02-13 15:47:39 -0800

    rewrite app to play nice

--- backend/app.py
+++ backend/app.py
@@ -2,23 +2,40 @@ import uvicorn
 
 from starlette.applications import Starlette
 from starlette.endpoints import HTTPEndpoint
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, HTMLResponse, PlainTextResponse
 from starlette.middleware.cors import CORSMiddleware
+from starlette.staticfiles import StaticFiles
+import sys
+from utils.yelp import scrape_yelp_biz, yelp_biz
 
-
-app = Starlette()
-
+app = Starlette(debug=True, template_directory='backend/static')
+app.mount('/static', StaticFiles(directory='backend/static'), name='static')
 app.add_middleware(
     CORSMiddleware, allow_origins=["*"], allow_headers=["*"], allow_methods=["*"]
 )
 
-
-@app.route("/hello/{name}")
+@app.route("/business/{name:path}")
 class Hello(HTTPEndpoint):
-    def get(self, request, name=None):
-        msg = f"Hello, {name}!" if name else "Hello!"
-        return JSONResponse({"message": msg}, status_code=200)
+    async def get(self, request):
+        name = request.path_params['name']
+        biz = scrape_yelp_biz(name, None)
+        return biz_to_json(biz)
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.route('/')
+async def homepage(request):
+    template = app.get_template('index.html')
+    content = template.render(request=request)
+    return HTMLResponse(content)
+
+
+def biz_to_json(biz: yelp_biz) -> str:
+    reviews = [r._asdict() for r in biz.reviews]
+    res = biz._asdict()
+    res['reviews'] = reviews
+    
+    return JSONResponse(res, status_code=200)
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=5432)
