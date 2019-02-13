6801fd21cc359ef9beea233c14575b1c5e76b534
Ivan Rudovol <ivan.rudovol@gmail.com>
2019-02-13 15:47:03 -0800

    rehaul the react components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,64 @@
 import React from 'react';
 import axios from 'axios';
 
+
+class Review extends React.Component {
+  render() {
+    const review = this.props.review;
+    return (
+      <tr>
+        <td>{review.yelp_rating}</td>
+        <td>{review.model_rating}</td>
+        <td>{review.text}</td>
+      </tr>
+    );
+  }
+}
+
+class Business extends React.Component {
+  render() {
+    if (!this.props.data) {
+      return null;
+    }
+    const data = this.props.data;
+  return (
+    <div>
+      <li>Business Name: {data.name}</li> 
+      <li>Yelp Rating (overall rating): {data.yelp_rating}</li> 
+      <li>Review Rating (scores of 20 most relevant reviews averaged out): {data.review_rating}</li> 
+      <li>Model Rating: (model predicted scores of 20 most relevant reviews averaged out): {data.model_rating}</li> 
+      <strong>Reviews</strong>
+      <Reviews reviews={data.reviews} />
+    </div>
+  );
+  }
+}
+
+
+class Reviews extends React.Component {
+  render() {
+    const reviews = this.props.reviews;
+    const listItems = reviews.map((review) =>
+    <tr>
+      <Review review={review} />
+    </tr>
+  );
+  return (
+    <table>
+        <tr>
+          <th>Yelp Rating</th>
+          <th>Model Rating</th>
+          <th>Review</th> 
+        </tr>
+      {listItems}
+    </table>
+    
+  );
+  }
+}
+
 export default class App extends React.Component {
-  state = {data: null, name: ''};
+  state = { data: null, name: '', reviews: {} };
 
   handleChange = event => {
     this.setState({ name: event.target.value });
@@ -10,47 +66,31 @@ export default class App extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    axios.get(`http://localhost:8000/hello/${this.state.name}`)
-    .then(response => this.setState({data:response.data}))
-    .catch(function(error) {
-     console.log('Fetch error: ' + error.message);
-   })
+    let url = encodeURIComponent(this.state.name);
+    axios.get(`http://localhost:5432/business/${url}`)
+      .then(response => this.setState({ data: response.data }))
+      .catch(function (error) {
+        console.log('Fetch error: ' + error.message);
+      })
   }
 
   render() {
-
-    // `name` is the current input value of the form.
-    const name = this.state.name;
-
-    // `data` is the response returned by the backend.
     const data = this.state.data;
-
-    // `message` will default to "Hello", but will be replaced with the "Hello {name}"
-    // value contained in the `data.message` payload.
-    var message = "Hello";
-
-    console.log(data);
-
-    // Assign the payload value if it exists
-    if (this.state.data !== null) {
-      message = this.state.data.message;
-    }
-
-
+    
     return (
       <div>
-      <strong>Name</strong>: {name}<br/>
-
-      <form onSubmit={this.handleSubmit}>
-      <label>
-      <strong>Enter name: </strong>
-      <input type="text" name="name" onChange={this.handleChange} />
-      </label>
-      <button type="submit">Submit</button>
-      </form>
-
-      <strong>Message</strong>: {message}<br/><br/>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <strong>Enter Yelp URL: </strong>
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-      )
-    }
+        <Business data={data} />
+      </div>
+      
+    )
   }
+}
