34832d604d5ccc9daebace8cf1777b762717fd09
Ivan Rudovol <ivan.rudovol@gmail.com>
2019-02-13 16:08:51 -0800

    add rendered frontend

---
+++ backend/static/precache-manifest.3f8cc36ba8480b9af0ecd95895b80626.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6626ed5ad523b7ad3a04",
+    "url": "/static/css/main.b1c2d2c0.chunk.css"
+  },
+  {
+    "revision": "6626ed5ad523b7ad3a04",
+    "url": "/static/js/main.6626ed5a.chunk.js"
+  },
+  {
+    "revision": "2d0e2afb9f84e8360784",
+    "url": "/static/js/1.2d0e2afb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a5a102c9bb77c22899e1b09943feed61",
+    "url": "/index.html"
+  }
+];
